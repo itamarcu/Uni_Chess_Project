c4b57b6dd2f680d00ff40b8bdf4b27d8c3948348
Itamar Curiel <itamarcu@gmail.com>
2018-02-19 20:34:32 +0200

    sorry sorry sorry forgot that

---
+++ PossibleMoveLogic.c
@@ -0,0 +1,15 @@
+
+#include <stdlib.h>
+#include "PossibleMoveLogic.h"
+
+
+PossibleMove *get_possible_moves(Game *game, int r1, int c1) {
+    PossibleMove *get_possible_moves = (PossibleMove *) malloc(32 * sizeof(PossibleMove));
+
+    //size is 32, remember!
+
+    for (int i = 0; i < 32; i++) {
+
+    }
+
+}
---
+++ PossibleMoveLogic.h
@@ -0,0 +1,11 @@
+#ifndef UNI_CHESS_PROJECT_POSSIBLEMOVELOGIC_H
+#define UNI_CHESS_PROJECT_POSSIBLEMOVELOGIC_H
+
+#include "Structs.h"
+
+/**
+ * @return an array with size 32
+ */
+PossibleMove *get_possible_moves(Game *game, int r1, int c1);
+
+#endif //UNI_CHESS_PROJECT_POSSIBLEMOVELOGIC_H
